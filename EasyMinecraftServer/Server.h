a03a420e97613cc8130224a7b20845794e59226d
Kurtis <kparsons24@gmail.com>
2025-09-23 17:39:19 -0400

    Prototypes in Server.h

--- EasyMinecraftServer/Server.h
+++ EasyMinecraftServer/Server.h
@@ -1,5 +1,29 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class Server
 {
+private:
+
+	string serverFolderPath;
+	bool serverRunning;
+	int playersOnline;
+	int serverVersion;
+
+
+public:
+
+	Server();
+
+	void startServer();
+
+	void stopServer();
+
+	void updateServer();
+
+
+
 };
 
