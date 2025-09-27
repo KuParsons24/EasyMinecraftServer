860833a4ccca6baedfd76d5ed87eb0364f4c321f
Kurtis <kparsons24@gmail.com>
2025-09-27 14:59:36 -0400

    Add better comments to Server.cpp

--- EasyMinecraftServer/Server.cpp
+++ EasyMinecraftServer/Server.cpp
@@ -34,22 +34,26 @@ void Server::startServer()
 
 void Server::updateServer()
 {
+	// Create HTTP request object
 	curlpp::Easy myRequest;
-
+	// Set target URL
 	myRequest.setOpt<curlpp::options::Url>("https://launchermeta.mojang.com/mc/game/version_manifest.json");
 
+	// Set output to write to a response object
 	std::ostringstream response;
-
 	curlpp::options::WriteStream ws(&response);
-
 	myRequest.setOpt(ws);
 
+	// Run request
 	myRequest.perform();
 
+	// Parse the response to a json object
 	json j = json::parse(response.str());
 
+	// Find latest version number
 	string latestVersion = j["latest"]["release"];
 
+	// Iterate to find the correct element containing the correct version manifest
 	for (json::iterator i = j["versions"].begin(); i != j["versions"].end(); ++i)
 	{
 		//std::cout << *i;
@@ -58,22 +62,27 @@ void Server::updateServer()
 		{
 			//std::cout << element;
 			std::cout << element["url"] << endl;
+			// Set target url to the version manifest
 			myRequest.setOpt<curlpp::options::Url>(element["url"]);
 		}
 	}
 
+	// Clear the response object
 	response.str("");
 	response.clear();
+	// Run request
 	myRequest.perform();
+	// Clear the json object and parse the new response
 	j.clear();
-
 	j = json::parse(response.str());
 
 
 	std::cout << j["downloads"]["server"]["url"] << endl;
 
+	// Set target url to the server.jar file
 	myRequest.setOpt<curlpp::options::Url>(j["downloads"]["server"]["url"]);
 
+	// Download the server.jar file and write to file
 	std::ofstream serverFile("server.jar", std::ios::binary);
 	if (serverFile.is_open())
 	{
@@ -83,6 +92,6 @@ void Server::updateServer()
 	}
 	else
 	{
-
+		std::cout << "File \"server.jar\" could not be opened." << endl;
 	}
 }
